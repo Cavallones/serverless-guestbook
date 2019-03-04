671140b21e6ca9931730025c566b70b256189c60
Cavallones <q.sofian@gmail.com>
2019-03-04 15:31:11 +0100

    Update guestbook.js
    pari pris

--- docs/guestbook.js
+++ docs/guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = 'https://service.us.apiconnect.ibmcloud.com/gws/apigateway/api/CHANGEME/guestbook';
+const apiUrl = 'https://3e4d1dcb.eu-gb.apiconnect.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
